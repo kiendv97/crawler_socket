3e01e64f5444e67df2a8e1c74207c2f77a4493f0
DvKien <kiendv9797@gmail.com>
2019-08-19 09:33:41 +0700

    fix

--- utils/Send_ding.js
+++ utils/Send_ding.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 async function sendToDingtalk(data) {
   if (data.toString().indexOf('Số còn') != -1) { 
+    console.log('Data1: ' + data.toString().indexOf('Số còn'));
+    console.log('Data2: ' + data.toString().indexOf('Số không còn'));
     axios
       .post(`https://oapi.dingtalk.com/robot/send?access_token=031011ad57ec1aaf5b46b7d13663a54a8997ed6d0ba248e40b25f12f2f6159f0`, {
         "msgtype": "text",
@@ -15,6 +17,8 @@ async function sendToDingtalk(data) {
       });
   }
   if (data.toString().indexOf('Số không còn') != -1) {
+    console.log('Data3: ' + data.toString().indexOf('Số còn'));
+    console.log('Data4: ' + data.toString().indexOf('Số không còn'));
     axios
       .post(`https://oapi.dingtalk.com/robot/send?access_token=f7495de50b7616d0f8e6b76ea8686667d2fcea4f70a7f485cab5f850f2ae3203`, {
         "msgtype": "text",
---
+++ utils/test.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const Conver = require('./Convert_content');
+async function sendToDingtalk(data) {
+  if (data.indexOf('Số còn') != -1) { 
+    // axios
+    //   .post(`https://oapi.dingtalk.com/robot/send?access_token=031011ad57ec1aaf5b46b7d13663a54a8997ed6d0ba248e40b25f12f2f6159f0`, {
+    //     "msgtype": "text",
+    //     "text": {
+    //       "content": data
+    //     }
+    //   })
+    //   .then(responses => {
+    //     if (responses.data.status) {
+    //       console.log('sendToDingTalk ok');
+    //     }
+    //   });
+  }
+  if (data.toString().indexOf('Số không còn') != -1) {
+      console.log('1');
+      
+    // axios
+    //   .post(`https://oapi.dingtalk.com/robot/send?access_token=f7495de50b7616d0f8e6b76ea8686667d2fcea4f70a7f485cab5f850f2ae3203`, {
+    //     "msgtype": "text",
+    //     "text": {
+    //       "content": data
+    //     }
+    //   })
+    //   .then(responses => {
+    //     if (responses.data.status) {
+    //       console.log('sendToDingTalk ok');
+    //     }
+    //   });
+  }
+
+}
+
+sendToDingtalk("ht_bh1_hanhn: 0774682286.cty 2\nSố không còn");
