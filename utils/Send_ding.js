1b092cddd81bfba7e486bf5bd028f9a490fd53ca
DvKien <kiendv9797@gmail.com>
2019-09-19 09:58:31 +0700

    .

--- utils/Send_ding.js
+++ utils/Send_ding.js
@@ -3,7 +3,7 @@ async function sendToDingtalk(data) {
   if (data.toString().indexOf('Số còn') != -1) { 
   data = data.replace('Số còn,','');
     axios
-      .post(`https://oapi.dingtalk.com/robot/send?access_token=91c589d960f2ac62d383474d9e6380c10fee3fb4e2ea544611c7d62b3ba96920`, {
+      .post(`https://oapi.dingtalk.com/robot/send?access_token=49dded8f78a76c8fd1c9040cc62f1223663d66525d5c1667720efd9cc9060f95`, {
         "msgtype": "text",
         "text": {
           "content": data
@@ -18,7 +18,7 @@ async function sendToDingtalk(data) {
   if (data.toString().indexOf('Số không còn') != -1) {
     data = data.replace('Số không còn','');
     axios
-      .post(`https://oapi.dingtalk.com/robot/send?access_token=ce54472f3e749628492a0c657f93081e9abfa9bfce83efae65a844ce73eddbe6`, {
+      .post(`https://oapi.dingtalk.com/robot/send?access_token=90ad6701b49972ff984d462ca783f190de2c84e9b70044225cffa5cee9e1d212`, {
         "msgtype": "text",
         "text": {
           "content": data
