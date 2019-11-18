f0d516528235de7dd23037c36649be9acf82851a
DvKien <kiendv9797@gmail.com>
2019-11-18 13:39:10 +0700

    .

--- utils/Convert_content.js
+++ utils/Convert_content.js
@@ -13,7 +13,7 @@ async function convert(newMobi, username) {
     var finalCheck = checkChangeDate + checkChangeMonth * 30 + checkChangeYear * 365
 
 
-    if (newMobi['Trạng thái'].toString().search('Số đang sử dụng') == 0 || newMobi['Số cấm tác động'].toString().indexOf('Không') == -1) {
+    if (newMobi['Trạng thái'].toString().search('Số ghép lô') == 0 || newMobi['Trạng thái'].toString().search('Số đang sử dụng') == 0 || newMobi['Số cấm tác động'].toString().indexOf('Không') == -1) {
         infoString += '\nSố không còn'
 
     } else {
