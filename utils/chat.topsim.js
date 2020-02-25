f942d3540b945af5e5fdf3597dc915a3f0f63d46
kiendv97 <kiendv9797@example.com>
2020-02-25 17:13:48 +0700

    fixbug

--- utils/chat.topsim.js
+++ utils/chat.topsim.js
@@ -1,5 +1,8 @@
 const axios = require('axios')
 
-export const api_chat =  (data) => {
+ const api_chat =  (data) => {
     axios.post('http://fanpage.topsim.vn/on-data-mobi', {data: data})
+}
+module.exports = {
+    api_chat
 }
