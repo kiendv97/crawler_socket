28fd62d76a57d663a5e0f03690faa62a73b800d0
kiendv97 <dangquangvu999@gmail.com>
2020-07-14 10:44:00 +0700

    debug server

--- index.js
+++ index.js
@@ -86,9 +86,15 @@ app.post('/setinfo_viettel', async function (req, res, next) {
     try {
 
         const paramsQuery = Object.assign({}, req.body);
+        console.log(paramsQuery);
+
         const ISDN = await ISDNModel.findOne({ keyword: paramsQuery.keyword });
         if (ISDN !== null) {
+            console.log(ISDN);
+
             const ISDNUpdate = await ISDNModel.updateOne({ keyword: paramsQuery.keyword }, { $set: { status: 1, reponsedAt: Date.now(), content: paramsQuery.content } });
+            console.log(ISDNUpdate);
+
             res.status(200).send({
                 status: 1,
                 result: ISDN.content
